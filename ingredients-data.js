7639ade6110793258a222cd1f1a3285afa60e83e
新 <74092973+mayday98060@users.noreply.github.com>
2026-05-31 03:56:53 +0800

    Update ingredients-data.js

--- ingredients-data.js
+++ ingredients-data.js
@@ -32,19 +32,19 @@ const INGREDIENTS_DATA = [
         source: "野外淡水流域\n營地魚塘、晴空麥田、亞述丘陵、夏爾鎮、銀翼基地、多貝雪山、密斯卡鎮"
     },
     {
-        name: "冰塊",
-        rarity: "rare-white",
-        rarityText: "普通",
-        image: "images/ingredients/ice.jpg",
-        description: "晶瑩剔透的冰塊。主要作為廚房料理的調味輔料，用來降低食物溫度或保持鮮度，是調配熱水、果汁和各式消暑冷飲的必備材料。",
-        source: "在寒冷的「多貝雪山」地圖採集野外冰塊，或者直接前往營地/快樂101的市政廳找市政商人購買。"
+        name: "河豚",
+        rarity: "rare-blue",
+        rarityText: "貴重",
+        image: "ingredients/河豚.png",
+        description: "習性:雜食性淡水魚\n一種很容易「生氣」的魚，同時也是一種很美味的魚，如果不怕中毒的話。",
+        source: "野外淡水流域\n營地魚塘、夏爾鎮郊、銀翼基地、加泰林地、灰熊山、紅水湖、雙路市、鬼影山、沉寂平原、荒土曠野、殘垣戈壁、烏茨礦場、極地冰原、夏雨公路、白樹高地、遠星城、秋日森林、沙石堡、紅杉鎮"
     },
     {
-        name: "鯉魚",
-        rarity: "rare-green",
-        rarityText: "優秀",
-        image: "images/ingredients/carp.jpg",
-        description: "常見的淡水魚類。肉質細嫩，是魚類料理的基礎食材。烘烤或清蒸後的鯉魚料理可以為倖存者提供穩定的全面屬性加成。",
-        source: "在任何淡水水域（如秋日森林的河流中），使用釣魚竿配合適當的魚餌進行釣魚獲得。"
+        name: "鯰魚",
+        rarity: "rare-white",
+        rarityText: "普通",
+        image: "ingredients/鯰魚.png",
+        description: "習性:肉食性淡水魚\n一種長鬍鬚的魚，肉食性，生長速度極快，可用於烹飪。",
+        source: "野外淡水流域\n營地魚塘、夏爾鎮、銀翼基地、加泰林地、紅水湖、鬼影山、荒土曠野、殘垣戈壁、烏茨礦場、極地冰原、夏雨公路、白樹高地、秋日森林、萊文黎明區、萊文市"
     }
 ];
